72e7f0a73e4572f5f9d8bee6d69ef559cb0812f6
Atef Attef <atefattef@Atefs-MacBook-Pro.local>
2019-09-10 14:03:34 -0400

    Adds HTML and javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+var name = 'Atef Attef'
+
+console.log(
+    `Hello, my name is ${name}`
+)
+
+var number = 1 
+console.log (`original number is ${number}` )
+console.log (number * 2 )
+console.log (number * 5 )
+console.log (number / number)
+console.log (number - 7)
